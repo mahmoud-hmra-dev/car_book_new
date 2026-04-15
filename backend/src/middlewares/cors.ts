a0c1c47fd19a4640845c1e96790f9fb901a26fe2
Mahmoud Hamra <Mahmoud.hmra.dev@gmal.com>
2026-04-15 22:05:02 +0300

    small fix

--- backend/src/middlewares/cors.ts
+++ backend/src/middlewares/cors.ts
@@ -8,16 +8,7 @@ import { isOriginAllowed } from '../utils/originHelper'
  * @type {cors.CorsOptions}
  */
 const CORS_CONFIG: cors.CorsOptions = {
-  origin(origin, callback) {
-    if (isOriginAllowed(origin)) {
-      callback(null, true)
-    } else {
-      const message = `Not allowed by CORS: ${origin}`
-      logger.error(message)
-      callback(new Error(message))
-    }
-  },
-  credentials: true,
+  origin: '*',
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
